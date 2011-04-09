c424660141fe767d294f97499a9bdeeeffa07f2d
Aruna <arunasri.42@gmail.com>
2011-04-09 09:24:01 -0400

    fixing deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 gem 'handy', '>= 0.0.15'
 
 set :stages, %w(staging production)
+require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
-
-server "173.255.234.137", :app, :web, :db, :primary => true
+server "173.230.134.171", :app, :web, :db, :primary => true
 
 set :user, 'deploy'
 set :keep_releases, 7
@@ -20,21 +20,12 @@ set (:deploy_to) { "/home/#{user}/apps/#{application}" }
 set :copy_remote_dir, "/home/#{user}/tmp"
 
 after "deploy:update_code",   "app:copy_config_files"
-after "deploy:update_code",   "app:bundle_install"
 
 namespace :app do
   desc "copy the app_config_production.yml file"
   task :copy_config_files,:roles => :app do
     run "cp -fv #{deploy_to}/shared/config/database.yml #{release_path}/config"
     run "cp -fv #{deploy_to}/shared/config/app_config_production.yml #{release_path}/config"
-  end
-end
-
-namespace :app do
-  desc "bundle install"
-  task :bundle_install, :roles => :app do
-    cmd = "bundle install --gemfile #{release_path}/Gemfile --path #{deploy_to}/shared/bundle --deployment --without development test"
-    sudo cmd
   end
 end
 
