568da14f4f5b1120ea7462174de5a3eb06046101
Aruna <arunasri.42@gmail.com>
2011-04-12 14:54:28 -0400

    fixing player error

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -37,7 +37,7 @@ jQuery(function($) {
 		id: "playing",
 		allowFullScreen: "true"
 	};
-	swfobject.embedSWF("http://www.youtube.com/v/" + currentVideoId + "&enablejsapi=1&playerapiid=playing" + "&rel=0&autoplay=0&egm=0&loop=0&fs=1&hd=0&showsearch=0&showinfo=0&iv_load_policy=3&cc_load_policy=1", "playing", "720", "405", "8", null, null, params, atts);
+	swfobject.embedSWF("http://www.youtube.com/v/" + currentVideoId + "&enablejsapi=1&playerapiid=playing" + "&rel=0&autoplay=0&egm=0&loop=0&fs=1&hd=0&showsearch=0&showinfo=0&iv_load_policy=3&cc_load_policy=1&version=3", "playing", "720", "405", "8", null, null, params, atts);
 
 });
 
